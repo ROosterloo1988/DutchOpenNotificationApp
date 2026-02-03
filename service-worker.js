eb8caf1373353a455136b6ccc9d9d44e05f59774
ROosterloo1988 <117975611+ROosterloo1988@users.noreply.github.com>
2026-02-04 00:00:47 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,18 @@
-const CACHE_NAME = "dutch-open-v1";
+// --- VERSIE BEHEER ---
+// VERANDER DIT NUMMER ALS JE EEN UPDATE PUSHT NAAR GITHUB! (Bijv: v1 -> v2)
+const CACHE_NAME = "dutch-open-v1.1"; 
+
 const urlsToCache = [
+  "./",
   "./index.html",
   "./icon.png",
-  "./manifest.json"
+  "./manifest.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js",
+  "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap",
+  "https://cdn-icons-png.flaticon.com/512/3068/3068351.png"
 ];
 
-// Installatie: Cache de basisbestanden
+// Installeren
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -14,23 +21,13 @@ self.addEventListener("install", event => {
   );
 });
 
-// Fetch: Probeer netwerk eerst, anders cache (zodat data altijd vers is!)
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
-});
-
-// Update: Verwijder oude caches als we de versie verhogen
+// Activeren (Oude cache opruimen)
 self.addEventListener("activate", event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
@@ -39,3 +36,26 @@ self.addEventListener("activate", event => {
   );
 });
 
+// Fetch (Network First, then Cache)
+// Dit zorgt ervoor dat hij altijd eerst probeert de nieuwste versie van internet te plukken
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Als we online zijn: stop de nieuwe versie ook direct in de cache
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, resClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request)) // Als we offline zijn: pak de cache
+  );
+});
+
+// Luister naar het bericht om direct te updaten (vanuit de knop in index.html)
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
